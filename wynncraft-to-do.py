b3e04a6e12145b51191090131d98fb94d81b3cd6
brixt01 <brixt01@outlook.com>
2022-09-24 16:05:11 +0100

    Request data and print

--- wynncraft-to-do.py
+++ wynncraft-to-do.py
@@ -1,1 +1,9 @@
+#import libraries
+import requests
+import json
 
+#get the data
+username = "brixt01"
+data = requests.get('https://api.wynncraft.com/v2/player/'+username+'/stats/').json()
+
+print(json.dumps(data, indent=4))
